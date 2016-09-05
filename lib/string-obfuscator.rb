e427946a15662df0fc187bdaca1005a3bb0a267b
Peter Graham <peterghm@gmail.com>
2016-09-05 11:46:08 -0400

    Implement string obfuscator

--- lib/string-obfuscator.rb
+++ lib/string-obfuscator.rb
@@ -1,1 +1,20 @@
 require "string-obfuscator/version"
+
+module StringObfuscator
+  # Obfuscate as close to 50% of the value as possible, without ever revealing
+  # more than 50%.
+  def self.obfuscate(value, min_obfuscation_percent: 0.5, max_unobfuscated_length: nil, obfuscation_character: "*")
+    return value unless value.is_a?(String)
+
+    obfuscate_length = (value.length * min_obfuscation_percent).ceil
+    visible_length = value.length - obfuscate_length
+
+    # If `max_unobfuscated_length` is set, ensure that we don't show more than that.
+    if !max_unobfuscated_length.nil? && visible_length > max_unobfuscated_length
+      visible_length = max_unobfuscated_length
+      obfuscate_length = value.length - visible_length
+    end
+
+    obfuscation_character * obfuscate_length + value[-visible_length..-1]
+  end
+end
--- spec/string_obfuscator_spec.rb
+++ spec/string_obfuscator_spec.rb
@@ -1,5 +1,53 @@
 describe StringObfuscator do
   it 'has a version number' do
-    expect(StringObfuscator::VERSION).not_to be nil
+    expect(described_class::VERSION).not_to be nil
+  end
+
+  describe ".obfuscate" do
+    context "with a long value" do
+      subject { described_class.obfuscate("Hello? It's me, Danilo") }
+
+      it "should return >= 50% obfuscated" do
+        expect(subject).to eq("*********** me, Danilo")
+      end
+    end
+
+    context "with a long value, and max_unobfuscated_length set" do
+      subject { described_class.obfuscate("Hello? It's me, Danilo", max_unobfuscated_length: 4) }
+
+      it "should return >= 50% obfuscated" do
+        expect(subject).to eq("******************nilo")
+      end
+    end
+
+    context "with a short value" do
+      subject { described_class.obfuscate("Boo")}
+
+      it "should return >= 50% obfuscated" do
+        expect(subject).to eq("**o")
+      end
+    end
+
+    context "with obfuscation_character set" do
+      subject { described_class.obfuscate("Hello? It's me, Danilo", obfuscation_character: "$") }
+
+      it "uses the given character to obfuscate" do
+        expect(subject).to eq("$$$$$$$$$$$ me, Danilo")
+      end
+    end
+
+    context "with an empty String" do
+      subject { described_class.obfuscate("") }
+
+      it { is_expected.to eq("") }
+    end
+
+    [123, nil, true].each do |value|
+      context "with a non-String value of #{value.to_s}" do
+        subject { described_class.obfuscate(value) }
+
+        it { is_expected.to eq(value) }
+      end
+    end
   end
 end
